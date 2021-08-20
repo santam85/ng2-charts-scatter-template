f0310a942bbf336556e8479af80cbecce42eef80
Marco Santarelli <marco@backbase.com>
2021-08-20 10:45:55 +0200

    Updated to v3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { ChartDataSets, ChartType, ChartOptions } from 'chart.js';
-import { Label } from 'ng2-charts';
+import { Component } from '@angular/core';
+import { ChartConfiguration, ScatterDataPoint } from 'chart.js';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-  // scatter
-  public scatterChartOptions: ChartOptions = {
-    responsive: true,
-  };
+  title = 'ng2-charts-demo';
 
-  public scatterChartData: ChartDataSets[] = [
+  public scatterChartDatasets: ChartConfiguration<'scatter'>['data']['datasets'] = [
     {
       data: [
         { x: 1, y: 1 },
         { x: 2, y: 3 },
         { x: 3, y: -2 },
         { x: 4, y: 4 },
-        { x: 5, y: -3, r: 20 },
+        { x: 5, y: -3},
       ],
       label: 'Series A',
       pointRadius: 10,
     },
   ];
-  public scatterChartType: ChartType = 'scatter';
 
-  constructor() { }
+  public scatterChartOptions: ChartConfiguration<'scatter'>['options'] = {
+    responsive: false,
+  };
 
-  ngOnInit() {
+  constructor() {
   }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { ChartsModule } from 'ng2-charts';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { NgChartsModule } from 'ng2-charts';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, ChartsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    NgChartsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
